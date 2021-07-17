5d6b98393d0e5ca5d8c3672ee3db872ca5592f38
FireSuperSaurus <42922954+FireSuperSaurus@users.noreply.github.com>
2021-07-17 18:43:11 +1000

    added js file
    added js file

---
+++ toggle.js
@@ -0,0 +1,17 @@
+// toggle_function
+function clickDrop() {
+    document.getElementById('drop').classList.toggle('show');
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.toggled')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
